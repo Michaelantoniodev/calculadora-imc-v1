7ae164053b5a228fdb9c7af240268b77b640116f
Michael Antonio <michaelantony20101@hotmail.com>
2022-09-21 10:10:43 -0300

    versão v1

---
+++ javascript/variaveis.js
@@ -0,0 +1,16 @@
+let nome = prompt ('Qual o seu nome?');
+let peso = prompt ('Quanto voçê pesa?');
+let altura = prompt ('Qual a sua altura?');
+let resultado =  parseFloat(peso / altura **2);
+
+alert ('Estamos calculando seu IMC...');
+
+let resultadoExposto = document.getElementById ('calculo');
+let nomeDigitado = document.getElementById ('nome');
+let pesoDigitado = document.getElementById ('peso');
+let alturaDigitada = document.getElementById ('altura')
+nomeDigitado.innerHTML = nome;
+pesoDigitado.innerHTML = peso;
+alturaDigitada.innerHTML = altura;
+resultadoExposto.innerHTML= (resultado.toFixed (2));
+
